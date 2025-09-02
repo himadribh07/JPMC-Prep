25da3ea3c439e2907f1c282b54acb6f87b39c2af
Himadri Bhattacharya <hbhattacharya3@gmail.com>
2025-09-03 00:17:09 +0530

    Day6

---
+++ Day6/coinChange.java
@@ -0,0 +1,31 @@
+package Day6;
+
+public class coinChange {
+    public static int coinChange(int[] coins, int amount) {
+        if(amount<1)
+            return 0;
+        int [] dp = new int [amount+1];
+
+        for(int i=1;i<=amount;i++){
+            dp[i]=Integer.MAX_VALUE;
+
+            for(int coin:coins){
+                if(coin<=i && dp[i-coin]!=Integer.MAX_VALUE){
+                    dp[i]= Math.min(dp[i],1+dp[i-coin]);
+                }
+            }
+        }
+
+        if(dp[amount]==Integer.MAX_VALUE)
+            return -1;
+
+        return dp[amount];
+        
+    }
+
+    public static void main(String[] args) {
+        int [] coins = {1,2,5};
+        int amount = 11;
+        System.out.println(coinChange(coins, amount));
+    }
+}
